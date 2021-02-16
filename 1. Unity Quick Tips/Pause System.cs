e216f55f833818a06fe0db82da6fd830c835a56e
Kongsittha Nimking <70092181+RAGNAGUS@users.noreply.github.com>
2021-02-16 20:19:43 +0700

    Pause System

---
+++ 1. Unity Quick Tips/Pause System.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Press Space to pause the game
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            //Pause the game
+            Time.timeScale = 0;
+        }
+
+        //Press R to resume the game
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            //Resume the game
+            Time.timeScale = 1;
+        }
+
+        //Press S to slowdown the game
+        if (Input.GetKeyDown(KeyCode.S)) 
+        {
+            //Slow motion
+            Time.timeScale = 0.25f;
+        }
+
+    }
+}
