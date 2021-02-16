dbd5e822f751c7240a1a0f6f2cfb915522cfd3b4
Kongsittha Nimking <70092181+RAGNAGUS@users.noreply.github.com>
2021-02-16 19:24:24 +0700

    Change Gameobject Position

---
+++ 1. Unity Quick Tips/Change Gameobject Position.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector3 startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //asign startPosition
+        startPosition = new Vector3(0, 0, 0);
+
+        //grab the current pos = (asign) new position
+        transform.position = new Vector3(0, 0, 0);
+
+        //grab the current pos = (asign) new position in Unity
+        transform.position = startPosition;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
