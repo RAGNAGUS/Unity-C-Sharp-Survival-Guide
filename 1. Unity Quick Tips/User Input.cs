7c01d0de71d372555c138a83852b9d7be47b8312
Kongsittha Nimking <70092181+RAGNAGUS@users.noreply.github.com>
2021-02-16 19:22:05 +0700

    User Input

---
+++ 1. Unity Quick Tips/User Input.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if space key pressed down
+        //print a message
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            Debug.Log("Space Key");
+        }
+
+        //if e key held down
+        //print a message
+        if (Input.GetKey(KeyCode.E))
+        {
+            Debug.Log("Holding E");
+        }
+
+        //if f key is lifted up
+        //print a message
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            Debug.Log("F lifted up");
+        }
+    }
+}
