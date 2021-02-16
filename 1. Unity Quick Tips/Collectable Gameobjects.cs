a71f55127d07d897bca1b5950ee58b6280456da8
Kongsittha Nimking <70092181+RAGNAGUS@users.noreply.github.com>
2021-02-16 20:13:56 +0700

    Add Comment for Collectable Setting

--- 1. Unity Quick Tips/Collectable Gameobjects.cs
+++ 1. Unity Quick Tips/Collectable Gameobjects.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Box Collider : Is Trigger
+//add Rigibody
+
 public class Collectable : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
+            //add points
+            //add power up ability
             Destroy(this.gameObject);
         }
     }
