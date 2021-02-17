b78e0237a3fc74beb792d6cb8117647ed0814886
Kongsittha Nimking <70092181+RAGNAGUS@users.noreply.github.com>
2021-02-17 21:36:55 +0700

    If-Statements

---
+++ 3. If-Statements/Challenge Master If-Statements P03.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int _point;
+    private bool _hasMessageBeenSent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _point += 10;
+            Debug.Log(_point);
+        }
+
+        if (_point >= 50 && _hasMessageBeenSent == false)
+        {
+            Debug.Log("You are awesome!");
+            _hasMessageBeenSent = true;
+        }
+    }
+}
---
+++ 3. If-Statements/Challenge Speed Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            speed += 5;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            speed -= 5;
+        }
+
+        if (speed > 20)
+        {
+            Debug.Log("Slow Down!, now your speed is " + speed);
+        }
+        else if (speed == 0) 
+        {
+            Debug.Log("Speed Up!, now your speed is " + speed);
+        }
+
+        if (speed < 0) 
+        {
+            speed = 0;
+        }
+    }
+}
