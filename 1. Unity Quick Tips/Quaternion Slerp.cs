5d5504da0fd852c7ef44bf1871d3dd3e9d33e325
Kongsittha Nimking <70092181+RAGNAGUS@users.noreply.github.com>
2021-02-17 15:44:11 +0700

    Quaternion Slerp

---
+++ 1. Unity Quick Tips/Quaternion Slerp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //direction = destination - source
+        Vector3 directionToFace = _target.position - transform.position;
+
+        Debug.DrawRay(transform.position, directionToFace, Color.green);
+
+        //access our current rotation = Quaternion Look Rotation
+        Quaternion targetRotation = Quaternion.LookRotation(directionToFace);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime);
+    }
+}
