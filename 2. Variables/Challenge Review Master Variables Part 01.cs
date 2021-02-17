5df30139107d5d4b046da972fa92f00e674f7fb3
Kongsittha Nimking <70092181+RAGNAGUS@users.noreply.github.com>
2021-02-17 18:05:22 +0700

    Variables

---
+++ 2. Variables/Challenge Review Master Variables Part 01.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChallengeReviewMasterVariablesPart01 : MonoBehaviour
+{
+    public string myName = "RAGNAGUS";
+    public int myAge = 26;
+    public float mySpeed = 10.2f;
+    public int health = 100;
+    public int score = 120;
+    public bool hasAllKeys; //Default is False
+    public int ammoCount = 30;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("My name is " + myName + " and I am " + myAge + " years old.");
+        Debug.Log("Speed: " + mySpeed);
+        Debug.Log("Health: " + health);
+        Debug.Log("Score: " + score);
+        Debug.Log("Has All Keys: " + hasAllKeys);
+        Debug.Log("Ammo Count: " + ammoCount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 2. Variables/Challenge Review Master Variables Part 02.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChallengeReviewMasterVariablesPart02 : MonoBehaviour
+{
+    public string itemName;
+    public string itemDescription;
+    public Sprite itemIcon;
+    public float attackStrenght;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Name: " + itemName);
+        Debug.Log("Description: " + itemDescription);
+        Debug.Log("Attack: " + attackStrenght);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
