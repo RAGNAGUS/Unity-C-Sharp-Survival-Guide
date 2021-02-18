67bb93be5f11c4b80425ee8ddf53552aea03f1a0
Kongsittha Nimking <70092181+RAGNAGUS@users.noreply.github.com>
2021-02-18 09:00:04 +0700

    Switch Statements

---
+++ 4. Switch Statements/Challenge Master Switch Statements P01.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int point;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            point = 50;
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            point = 100;
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            point = 0;
+        }
+
+        switch (point)
+        {
+            case 50:
+                Debug.Log("Points are 50!");
+                break;
+            case 100:
+                Debug.Log("Points are 100!");
+                break;
+            default:
+                Debug.Log("You need 50 or 100 points to recieve a message");
+                break;
+        }
+    }
+}
---
+++ 4. Switch Statements/Challenge Master Switch Statements P02.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject cube;
+    private int _colorSelect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _colorSelect = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _colorSelect = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            _colorSelect = 3;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            _colorSelect = 4;
+        }
+
+        switch (_colorSelect)
+        {
+            case 1:
+                cube.GetComponent<Renderer>().material.color = Color.blue;
+                break;
+            case 2:
+                cube.GetComponent<Renderer>().material.color = Color.red;
+                break;
+            case 3:
+                cube.GetComponent<Renderer>().material.color = Color.green;
+                break;
+            case 4:
+                cube.GetComponent<Renderer>().material.color = Color.black;
+                break;
+            default:
+                Debug.Log("Invalid Selection");
+                break;
+        }
+    }
+}
