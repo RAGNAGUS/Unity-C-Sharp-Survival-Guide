502bf16f8131e85a350938affd2bcd731a1da7ed
Kongsittha Nimking <70092181+RAGNAGUS@users.noreply.github.com>
2021-02-20 16:45:36 +0700

    Properties

---
+++ 10. Properties/Challenge Declaring Properties 1.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test
+{
+    GameManager gm = new GameManager();
+
+    public Test()
+    {
+        Debug.Log(gm.Speed); //Able to read speed value
+
+        //gm.Speed = 5; //Not able to edit speed value
+    }
+}
+public class GameManager : MonoBehaviour
+{
+    private float _speed;
+
+    public float Speed
+    {
+        get
+        {
+            return _speed;
+        }
+        private set
+        {
+            _speed = value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Speed = 10f;
+        Debug.Log(Speed);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ 10. Properties/Challenge Declaring Properties 2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test
+{
+    GameManager gm = new GameManager();
+
+    public Test()
+    {
+        gm.Name = "RAGNAGUS";
+        Debug.Log(gm.Name);
+    }
+
+}
+public class GameManager : MonoBehaviour
+{
+    private string _name;
+    public string Name
+    {
+        get
+        {
+            return _name;
+        }
+        set
+        {
+            _name = value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
