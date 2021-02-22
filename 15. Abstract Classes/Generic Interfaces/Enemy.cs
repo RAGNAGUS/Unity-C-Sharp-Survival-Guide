a3c6d72a4f538dd2bb1f3a7a6c0461f22db81752
Kongsittha Nimking <70092181+RAGNAGUS@users.noreply.github.com>
2021-02-22 18:03:04 +0700

    Error Fix

--- 15. Abstract Classes/Generic Interfaces/Enemy.cs
+++ 15. Abstract Classes/Generic Interfaces/Enemy.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour, IDamagable<int>, IDamagable<GameObject> //Generic type
+public class Enemy : MonoBehaviour, IDamagable<int> //IDamagable<GameObject> //Generic type
 {
     public int Health { get; set; }
 
